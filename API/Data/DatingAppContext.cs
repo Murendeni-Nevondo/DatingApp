74a6222a5bfc293cafd6372e665bc37fd102bbc6
Murendeni Nevondo <mnevondo727@gmail.com>
2023-02-06 13:34:53 +0200

    Initial Commit

---
+++ API/Data/DatingAppContext.cs
@@ -0,0 +1,13 @@
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class DatingAppContext : DbContext
+    {
+        public DatingAppContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
